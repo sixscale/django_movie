a280dc10a5535a2daa3eb9fddcfd5beb224cab7c
Никита Рябов <gfgfgf354@gmail.com>
2023-10-09 14:21:27 +0300

    init commit

---
+++ django_movie/Movie/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+
+
+
+#class ReviewAdmin(admin.ModelAdmin):
+
+
+# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre)
+# admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
